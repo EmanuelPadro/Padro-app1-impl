26352a91e2174a9e1197e9b0ab5d4ea4c32191f4
EmanuelPadro <emanuelpadro@knights.ucf.edu>
2021-11-07 00:25:11 -0400

    Added pseudocode for the new class

--- src/main/java/baseline/ListOperations.java
+++ src/main/java/baseline/ListOperations.java
@@ -1,12 +1,19 @@
 package baseline;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ListOperations {
-
-    public static void CreateList(){
+    /*This class is here mostly as backup because in the future I believe
+    I will need a class to manage loading and saving lists or perhaps even
+    handle multiple lists
+    */
+    void saveList(){
+        //will save a lit to a file
     }
 
+    void loadList(){
+        //will load a list to a file
+    }
 
+    void restoreList(){
+        //will restore a created list???
+    }
 }
